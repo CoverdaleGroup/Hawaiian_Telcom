ba71508ed2b9645473e18b15d2d97cf4fb4c872b
Kyle Patrick <kyle_patrick@coverdalegroup.com>
2017-09-12 16:36:31 -0400

    Initial commit

---
+++ app/lib/oc/headerNavigation.js
@@ -0,0 +1,46 @@
+angular.module('OrderCloud-HeaderNavigation', []);
+angular.module('OrderCloud-HeaderNavigation')
+    .directive('headernavigation', headernavigation)
+    .controller('HeaderNavigationCtrl', HeaderNavigationCtrl)
+;
+
+function headernavigation() {
+    return {
+        restrict: 'E',
+        template: template
+    };
+
+    function template() {
+        return [
+            '<section class="header-navigation">',
+            '<div class="row">',
+            '<div class="col-xs-12 col-sm-6 pull-right">',
+            '<ul>',
+            '<li><a href="contactus">Contact Us</a></li>',
+            '<li><a href="admin">Account</a></li>',
+            '<li><a href="cart">Cart&nbsp;',
+            '<span ng-if="currentOrder.LineItems.length" ng-bind="cartCount" class="badge"></span>',
+            '</a></li>',
+            '<li><a ng-click="Logout()">Log Out</a></li>',
+            '</ul>',
+            '</div>',
+            '</div>',
+            '</section>'
+        ].join('');
+    }
+}
+
+HeaderNavigationCtrl.$inject = ['$scope','User'];
+function HeaderNavigationCtrl($scope, User) {
+
+    $scope.Logout = function(){
+        User.logout();
+        if ($scope.isAnon) {
+            $location.path("/catalog");
+            User.login(function(user) {
+                $scope.user = user;
+            });
+        }
+    };
+
+}
